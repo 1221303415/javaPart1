d349802fd27bfb8ac5681d2a5f4ba0eb781a2f15
G3_T13L <124128597+1221303415@users.noreply.github.com>
2024-01-06 22:01:58 +0800

    User.java

---
+++ User.java
@@ -0,0 +1,22 @@
+public class User {
+    private int id;
+    private String name;
+    private String userType;
+
+    public User(int id, String name, String userType){
+        this.id = id;
+        this.name = name;
+        this.userType = userType;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getPassword() {
+        return name;
+    }
+    public String getUserType() {
+        return userType;
+    }
+
+
+}
