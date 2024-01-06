ccd487de9bac7e5e93b0e8080bbd0da2a90e1cce
G3_T13L <124128597+1221303415@users.noreply.github.com>
2024-01-06 22:05:54 +0800

    Lecturer.java

---
+++ Lecturer.java
@@ -0,0 +1,20 @@
+public class Lecturer extends User {
+    private ArrayList<String> assignCourses;
+
+    public Lecturer(int id, String name, String assignCourse) {
+        super(id, name, "Lecturer");
+        this.assignCourses = new ArrayList<>(Arrays.asList(assignCourse));
+    }
+
+    public void addCourse(String course) {
+        assignCourses.add(course);
+    }
+
+    public ArrayList<String> getAssignCourses() {
+        return assignCourses;
+    }
+
+    public String toString(){
+        return "Lecturer: " + getPassword();
+    }
+}
